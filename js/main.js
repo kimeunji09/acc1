d8f8bafaa033b20819526d586e60c9587ba28e51
kimeunji09 <stella000912@gmail.com>
2022-09-16 00:29:29 +0900

    jestina project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//좌우슬라이딩
+setInterval(function(){
+  $('#slide1>ul').delay(2500);
+  $('#slide1>ul').animate({marginLeft: "-1200px"})
+  $('#slide1>ul').delay(2500);
+  $('#slide1>ul').animate({marginLeft: "-2400px"})
+  $('#slide1>ul').delay(2500);
+  $('#slide1>ul').animate({marginLeft: "0px"})
+});
+

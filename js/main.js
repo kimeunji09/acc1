eb22c1e77650ab28657305768724089645e29c36
kimeunji09 <stella000912@gmail.com>
2022-09-29 17:03:12 +0900

    portpolio project

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-//좌우슬라이딩
-setInterval(function(){
-  $('#slide1>ul').delay(2500);
-  $('#slide1>ul').animate({marginLeft: "-1200px"})
-  $('#slide1>ul').delay(2500);
-  $('#slide1>ul').animate({marginLeft: "-2400px"})
-  $('#slide1>ul').delay(2500);
-  $('#slide1>ul').animate({marginLeft: "0px"})
-});
+// 메뉴
+$('header nav ul li').hover(function(){
+  $(this).find('.sub').stop().slideDown();
+},function(){
+  $(this).find('.sub').stop().slideUp();
+})
 
+// 슬라이드 페이드인아웃
+$('#slide1>ul>li').hide();
+$('#slide1>ul>li:first-child').show();
+
+setInterval(function(){
+    $('#slide1>ul>li:first-child').fadeOut()
+    .next().fadeIn().end(1000)
+    .appendTo('#slide1>ul');
+},3000);
+
+//가장 인기 많은 상품 영역
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
+});
